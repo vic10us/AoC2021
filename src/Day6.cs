6826688525577674d7a8b563abdedaa544c156af
vic10us <code@dexs.ca>
2021-12-06 20:39:30 -0500

    Adding Day 6

---
+++ src/Day6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AoC2021;
+
+public static class Day
+{
+    private static Task PartOne()
+    {
+        Console.WriteLine("-- Part 1 --");
+        return Task.CompletedTask;
+    }
+
+    private static Task PartTwo()
+    {
+        Console.WriteLine("-- Part 2 --");
+        return Task.CompletedTask;
+    }
+
+    public static async Task Run()
+    {
+        Console.WriteLine("Advent of Code 2021, Day 4!");
+        await PartOne();
+        await PartTwo();
+        Console.WriteLine("");
+    }
+}
